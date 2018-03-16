a73f69efd4540a3dbdd088286aa96dea68b19b1c
ibnibrahim <aliouibnibrahim@gmail.com>
2018-03-16 15:17:31 +0000

    Script permettant d'uploader un fichier excel contenant les événements

--- Evenement.php
+++ Evenement.php
@@ -1,7 +1,7 @@
 <?php
 
 	try {
-		$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+		$bdd = new PDO('mysql:host=localhost;dbname=Gestion_de_Tournoi;charset=utf8', 'root', '');
 	}
 	catch (Exception $e) {
 	        die('Erreur : ' . $e->getMessage());
