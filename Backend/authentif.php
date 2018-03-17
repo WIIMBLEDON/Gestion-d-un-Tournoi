60e3f3cefd51e21b755dbba0146ffed3c95b323a
cheikhmbacke <serignecheikhmbackethiaw@gmail.com>
2018-03-17 15:25:13 +0000

    Derniére version du fichier authentification

--- Backend/authentif.php
+++ Backend/authentif.php
@@ -3,30 +3,33 @@ session_start();
   $user=$_REQUEST["user"];
   $password=$_REQUEST["passwd"];
   if (!empty($user)) {
-    if (!empty($passwd)) {
-      switch ($passwd) {
+    if (!empty($password)) {
+      switch ($password) {
         case 'arbitre':
-          header("Location:refreeHome.php");
+          header("Location:../ajaxcup_master/refreeHome.php");
           break;
         case 'officielle':
-          header("Location:officeHome.php");
+          header("Location:../ajaxcup_master/officeHome.php");
           break;
         case 'coach':
-          header("Location:coachHome.php");
+          header("Location:../ajaxcup_master/coachHome.php");
           break;
 
         default:
          $_SESSION['msg']="Le mot de passe entrer n'existe pas";
-         header("Location:index_en.html");
+         header("Location:../ajaxcup_master/index_en.php");
+         // echo $password."<br><br>";
           break;
       }
     }else {
       $_SESSION['msg']="Veuiller entrer un mot de passe";
-      header("Location:index_en.html");
+      header("Location:../ajaxcup_master/index_en.php");
+      // echo $password."<br><br>";
     }
   }else {
     $_SESSION['msg']="Veuiller entrer l'identifiant";
-    header("Location:index_en.html");
+    header("Location:../ajaxcup_master/index_en.php");
+    // echo $password."<br><br>";
   }
 
  ?>
