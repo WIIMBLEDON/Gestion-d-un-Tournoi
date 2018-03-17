817d2425fbd9d181999746a9bcfb1a5bc0125761
cheikhmbacke <serignecheikhmbackethiaw@gmail.com>
2018-03-17 15:38:59 +0000

    Modification du dossier fichier fichier

--- listPlayers.php
+++ listPlayers.php
@@ -3,14 +3,13 @@ include "db.php";
 
 $team = urldecode($_GET['team']);
 echo "<table>";
-$req = $bdd->prepare('SELECT * FROM joueur WHERE nom_equipe = ?');
+$req = $bdd->prepare('SELECT * FROM joueur WHERE Nom_equipe = ?');
 $req->execute(array($team));
 while($donnees = $req->fetch()){
-    echo "<tr>
-                <td><a href=infoPlayer.php?player=".urlencode($donnees['nom']).">$donnees[prenom] $donnees[nom]</a></td>
-                <td>$donnees[nationalite]</td>
-                <td>$donnees[age]</td>
-                <td>$donnees[taille]</td>
+    echo "<tr align="center">;
+                <td><a href=infoPlayer.php?player=".urlencode($donnees['Nom']).">$donnees[Prenom] $donnees[Nom]</a></td>
+                <td>$donnees[Nationalite]</td>
+                <td>$donnees[Age]</td>
             </tr>    
             ";
 }
--- listTeam.php
+++ listTeam.php
@@ -6,8 +6,8 @@ echo "<table>";
 $resp =$bdd->query('SELECT * FROM equipe');
 while($donnees = $resp->fetch()){
     echo "<tr>
-                <td><a href=listPlayers.php?team=".urlencode($donnees['nom_equipe']).">$donnees[nom_equipe]</a></td>
-                <td>$donnees[stade]</td>
+                <td><a href=listPlayers.php?team=".urlencode($donnees['Nom_equipe']).">$donnees[Nom_equipe]</a></td>
+                <td></td>
             </tr>    
             ";
 }
